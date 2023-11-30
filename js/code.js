3694c74577de6b6311f2f709944bf114c8ef398a
hassan <hassanak509@gmail.com>
2023-11-30 15:37:21 +0200

    links to facebook,insta,whatsapp and location

--- js/code.js
+++ js/code.js
@@ -1,16 +1,42 @@
 let prom_sec = document.getElementById("prom-sec")
 let info_sec = document.getElementById("info-sec")
 let abt_sec = document.getElementById("abt-sec")
+let cont_sec = document.getElementById("cont-sec");
 
 
 function show_home(){
     prom_sec.style.display = 'block'
     info_sec.style.display = 'flex'
     abt_sec.style.display = 'none'
+    cont_sec.style.display = 'none'
 }
 
 function show_about(){
     prom_sec.style.display = 'none'
     info_sec.style.display = 'none'
     abt_sec.style.display = 'block'
+    cont_sec.style.display = "none";
+}
+
+function show_contact(){
+    prom_sec.style.display = "none";
+    info_sec.style.display = "none";
+    abt_sec.style.display = "none";
+    cont_sec.style.display = "block";
+}
+
+function to_facebook(){
+    window.location.href = "https://www.facebook.com/";
+}
+
+function to_instagram(){
+    window.location.href = "https://www.instagram.com/";
+}
+
+function to_whatsapp(){
+    window.location.href = "https://www.facebook.com/";
+}
+
+function to_location(){
+    window.location.href = "https://maps.app.goo.gl/rhe2unvHArqNKYh38";
 }
